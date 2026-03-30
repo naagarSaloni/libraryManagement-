aea8757bbd14f0698a05bdf65e13f0d5f762a7d5
naagarSaloni <141541093+naagarSaloni@users.noreply.github.com>
2026-03-30 12:57:28 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+   import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Register from './pages/Register.jsx'
 import Login from './pages/Login.jsx'
+import Books from './pages/Books.jsx'
+import Longues from './pages/Longues.jsx'
+import Seat from './pages/Seat.jsx'
+import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,6 +15,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Route path="/" element={<App />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/books" element={<Books />} />
+      <Route path="/longues" element={<Longues />} />
+      <Route path="/seat" element={<Seat />} />
     </Routes>
   </BrowserRouter>
 )
